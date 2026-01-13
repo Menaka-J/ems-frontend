8781409e30a69341626ca0d001452c763e7963cc
Menaka J <menakaraman4@gmail.com>
2026-01-13 20:44:16 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ListEmployeeComponent from './Component/ListEmployeeComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <ListEmployeeComponent />
     </>
   )
 }
--- src/Component/ListEmployeeComponent.jsx
+++ src/Component/ListEmployeeComponent.jsx
@@ -2,11 +2,58 @@ import React from 'react'
 
 function ListEmployeeComponent() {
 
-    
+    const dummy = [
+        {
+            "id": 1,
+            "fname": "Dean",
+            "lname": "Winchester",
+            "email": "dean@gmail.com"
+        },
+        {
+            "id": 2,
+            "fname": "Sam",
+            "lname": "Winchester",
+            "email": "sam@gmail.com"
+        }, {
+            "id": 3,
+            "fname": "John",
+            "lname": "Winchester",
+            "email": "john@gmail.com"
+        }, {
+            "id": 4,
+            "fname": "Mary",
+            "lname": "Winchester",
+            "email": "mary@gmail.com"
+        },
+    ]
     return (
-        <div>
+        <>
+            <div>
+                <h2>List of Employees</h2>
 
-        </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Employee Id</th>
+                            <th>Employee First Name</th>
+                            <th>Employee Last Name</th>
+                            <th>Employee Email</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {dummy.map((employee) => {
+                                <tr key={employee.id}>
+                                    <td>{employee.id}</td>
+                                    <td>{employee.fname}</td>
+                                    <td>{employee.lname}</td>
+                                    <td>{employee.email}</td>
+                                </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
