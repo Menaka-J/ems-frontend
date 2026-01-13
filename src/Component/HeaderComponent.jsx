7dfdd8fbb394b767ac97d9497ee35432492e384c
Menaka J <menakaraman4@gmail.com>
2026-01-13 21:08:48 +0530

    commitnew 4

--- src/Component/HeaderComponent.jsx
+++ src/Component/HeaderComponent.jsx
@@ -7,7 +7,7 @@ function HeaderComponent() {
         <div>
 
             <header>
-                <nav className='navbar navbar-light bg-light'> 
+                <nav className='navbar navbar-dark bg-dark'> 
                     <a className="navbar-brand" href="#">Employee Management System</a>
                 </nav>
             </header>
